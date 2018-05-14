f742814ac3e49a8f3d8217e7b68c7ade6ea7d04a
Bahman Movaqar <Bahman@BahmanM.com>
2018-05-14 04:49:40 +0200

    reformat

--- tests/test-p1.lisp
+++ tests/test-p1.lisp
@@ -13,19 +13,19 @@
 (in-suite p1-test-suite)
 
 (test multiple-of-3-or-5p
-  (is-true (euler/p1::multiple-of-3-or-5p 3))
-  (is-true (euler/p1::multiple-of-3-or-5p 5))
-  (is-true (euler/p1::multiple-of-3-or-5p 15))
-  (is-false (euler/p1::multiple-of-3-or-5p 7)))
+      (is-true (euler/p1::multiple-of-3-or-5p 3))
+      (is-true (euler/p1::multiple-of-3-or-5p 5))
+      (is-true (euler/p1::multiple-of-3-or-5p 15))
+      (is-false (euler/p1::multiple-of-3-or-5p 7)))
 
 (test solution*
-  (is (= 0 (euler/p1::solution* 1 0)))
-  (is (= 23 (euler/p1::solution* 9 0)))
-  (is (= 33 (euler/p1::solution* 10 0)))
-  (is (= 53 (euler/p1::solution* 11 20))))
+      (is (= 0 (euler/p1::solution* 1 0)))
+      (is (= 23 (euler/p1::solution* 9 0)))
+      (is (= 33 (euler/p1::solution* 10 0)))
+      (is (= 53 (euler/p1::solution* 11 20))))
 
 (test solution
-  (is (= 0 (euler/p1:solution 2)))
-  (is (= 23 (euler/p1:solution 10)))
-  (is (= 33 (euler/p1:solution 11)))
-  (is (= 33 (euler/p1:solution 12))))
+      (is (= 0 (euler/p1:solution 2)))
+      (is (= 23 (euler/p1:solution 10)))
+      (is (= 33 (euler/p1:solution 11)))
+      (is (= 33 (euler/p1:solution 12))))
