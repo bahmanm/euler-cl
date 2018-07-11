5dc891f3ce5a2c43c291917df718f577aa81e4f4
Bahman Movaqar <Bahman@BahmanM.com>
2018-07-11 08:55:58 +0200

    use caching for better performance
    Almost 3x faster execution now

--- src/p14.lisp
+++ src/p14.lisp
@@ -7,18 +7,25 @@
 (in-package :euler/p14)
 
 
-(defun count-chain-terms (n)
-  (do ((n n (if (evenp n) (/ n 2) (+ (* 3 n) 1)))
-       (result 1 (1+ result)))
-      (nil)
-    (when (= n 1) (return result))))
-
+(defun count-chain-terms (n &optional (cache (make-hash-table)))
+  (do* ((m n
+           (if (evenp m)
+               (/ m 2)
+               (+ (* 3 m) 1)))
+        (cached (gethash m cache)
+                (gethash m cache))
+        (result (or cached 1)
+                (+ result (or cached 1))))
+       ((or cached (= m 1)) (setf (gethash n cache)
+                                   result))))
 
 (defun solution (&optional (n 1000001))
   (let ((max-terms-count 0)
-        (max-terms-number 0))
+        (max-terms-number 0)
+        (cache (make-hash-table)))
     (do* ((i 1 (1+ i))
-          (current-terms-count (count-chain-terms i) (count-chain-terms i)))
+          (current-terms-count (count-chain-terms i cache)
+                               (count-chain-terms i cache)))
          ((> i n))
       (when (> current-terms-count max-terms-count)
         (setf max-terms-count current-terms-count)
