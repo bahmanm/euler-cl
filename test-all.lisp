0dd659c65852efabbb4be9c609f78e532891cc51
Bahman Movaqar <bahman@bahmanm.com>
2024-02-28 19:19:33 -0800

    Remove unused files (#16)

--- test-all.lisp
+++
@@ -1,8 +0,0 @@
-(setf asdf:*central-registry*
-      (list* '*default-pathname-defaults*
-             (car (directory #p"."))
-             asdf:*central-registry*))
-(asdf:load-system :euler)
-(asdf:load-system :euler-tests)
-(asdf:test-system :euler)
-(exit)
