6cb2f8c31d584b8e324288e80cb3e1693e29d049
Bahman Movaqar <bahman@bahmanm.com>
2024-03-10 16:05:22 -0700

    Implement pandigital check

--- src/p38.lisp
+++ src/p38.lisp
@@ -7,7 +7,24 @@
 (in-package :euler/p38)
 
 (defun pandigitalp (n)
-  nil)
+  (let* ((digits (make-hash-table :size 9)))
+    (do* ((x n
+             (floor (/ x 10)))
+          (digit (mod x 10)
+                 (mod x 10))
+          (seen-digit-p (gethash digit digits nil)
+                        (gethash digit digits nil)))
+         (nil)
+      (cond
+        ((zerop x) (return t))
+        ((zerop digit) (return nil))
+        (seen-digit-p (return nil))
+        (t (setf (gethash digit digits)
+                 t))))
+    (do* ((i 1 (1+ i)))
+         ((= i 10) t)
+      (unless (gethash i digits)
+        (return nil)))))
 
 (defun solution ()
   nil)
